021f9cf14286512f63cf67c1ec210f2419afb904
Matheus <matheus.matsuo@edu.unifil.br>
2022-06-17 21:46:08 -0300

    Arrumando conexão banco h2

--- Backend/redlack/src/main/java/com/redlack/redlack/controllers/UsuarioController.java
+++ Backend/redlack/src/main/java/com/redlack/redlack/controllers/UsuarioController.java
@@ -1,14 +1,14 @@
 package com.redlack.redlack.controllers;
 
 import com.redlack.redlack.dto.UsuarioDTO;
-import com.redlack.redlack.entities.Usuario;
-import com.redlack.redlack.repositories.UsuarioRepository;
 import com.redlack.redlack.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/usuarios")
@@ -18,25 +18,25 @@ public class UsuarioController {
     private UsuarioService service;
 
     @GetMapping
-    public ResponseEntity<List<UsuarioDTO>> findAll(){
+    public ResponseEntity<List<UsuarioDTO>> findAll() {
         List<UsuarioDTO> result = service.findAll();
         return ResponseEntity.ok(result);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id){
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id) {
         UsuarioDTO result = service.findById(id);
         return ResponseEntity.ok().body(result);
     }
 
     @GetMapping(value = "/nome/{nome}")
-    public ResponseEntity<UsuarioDTO> findByNome(@PathVariable String nome){
+    public ResponseEntity<UsuarioDTO> findByNome(@PathVariable String nome) {
         UsuarioDTO result = service.findByNome(nome);
         return ResponseEntity.ok().body(result);
     }
 
     @GetMapping(value = "/email/{email}")
-    public ResponseEntity<UsuarioDTO> findByEmail(@PathVariable String email){
+    public ResponseEntity<UsuarioDTO> findByEmail(@PathVariable String email) {
         UsuarioDTO result = service.findByEmail(email);
         return ResponseEntity.ok().body(result);
     }
@@ -48,13 +48,15 @@ public class UsuarioController {
     }
 
     @DeleteMapping(value = "/deletar/{id}")
-    public void delete(@PathVariable Long id){
-         service.deleteById(id);
+    public void delete(@PathVariable Long id) {
+        service.deleteById(id);
     }
 
-    @PutMapping(value = "/{id}")
-    public void update(@PathVariable Long id, @RequestBody UsuarioDTO dto){
+    @PutMapping(value = "/editar/{id}")
+    public void update(@PathVariable Long id, @RequestBody UsuarioDTO dto) {
         service.update(id, dto);
     }
-        }
+
+}
+
    
--- Backend/redlack/src/main/resources/import.sql
+++ Backend/redlack/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into tb_usuario(nome, email) values('Matheus', 'matheus@teste.com');
+insert into tb_usuario(nome, email, senha) values('Matheus', 'matheus@teste.com', 'musculo');
 insert into tb_usuario(nome, email) values('João', 'joão@teste.com');
 insert into tb_usuario(nome, email) values('Gal', 'gal@teste.com');
-insert into tb_usuario(nome, email) values('Daniel', 'daniel@teste.com');
+insert into tb_usuario(nome, email, senha) values('Daniel', 'daniel@teste.com', 'paozinho');
 insert into tb_usuario(nome, email) values('Felipe', 'felipe@teste.com');
